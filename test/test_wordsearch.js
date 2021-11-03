8e9ca9cd94f02fa7464002a9f3df7ada54390250
Awab Osman <awabosman3@gmail.com>
2021-11-03 18:05:11 -0400

    word search

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,7 +4,7 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
-  it("should return false if the word is not present", function() {
+  it("should return false if the FRANK is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -19,8 +19,31 @@ describe("#wordSearch()", function() {
 
     assert.isFalse(result);
   });
+  
+  it("should return true if the 241SHEEP! is present", function() {
+    const result = wordSearch([
+      ['2', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['4', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['1', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['H', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['E', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['E', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['P', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['!', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '241SHEEP!')
 
-  it("should return true if the word is present", function() {
+    assert.isTrue(result);
+  });
+
+  it("should return false if the TANKER is not present", function() {
+    const result = wordSearch(
+      [], 'TANKER')
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the SEINFELD is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = require("/home/labber/lighthouse/w2/d2/matrix_transposition.js")
+const wordSearch = (letters, word) => {    /// letters is the matrix and word is the match youre looking for
+    const horizontalJoin = letters.map(function(ls) {
+        return ls.join('');
+    });
+    
+    for (const line of horizontalJoin) {
+        if (line.includes(word)) {
+            return true; 
+        }
     }
-}
+///-------------------------------------------------
+    let matrix = transpose(letters);
+    const VerticalJoin = matrix.map(function(ls) {
+        return ls.join('');
+    });
+
+    for (const line of VerticalJoin) {
+        if (line.includes(word)) {
+            return true;   
+        }
+    }
+
+    return false;
+};
 
 module.exports = wordSearch
